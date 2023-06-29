491ad4dae54b1df1e725236b014fa3134a849954
abhinand5ai <abhinandsainasari@gmail.com>
2023-06-29 01:36:43 -0700

    basics

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
+use std::io;
+
 fn main() {
-    let a = 2;
-    let result = stack_only(a);
-    dbg!(result);
+    let mut input = String::new();
+    println!("Enter your weight: ");
+    io::stdin().read_line(&mut input).unwrap();
+    let weight: f32 = input.trim().parse().unwrap();
+    let mut mars_weight = calculate_weight_on_mars(weight);
+    mars_weight = mars_weight * 1000.0;
+    println!("Weight on Mars {} kg", mars_weight)
 }
 
-fn stack_only(b: i32) -> i32 {
-    let c = 3;
-    return b + c + stack_and_heap();
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
 
-fn stack_and_heap() -> i32 {
-    let d = 5;
-    let e = Box::new(7);
-    return d + *e;
+fn borrow_string(s: &String) {
+    println!("{}", s)
+}
+
+fn own_string(s: String){
+    println!("{}", s);
 }
